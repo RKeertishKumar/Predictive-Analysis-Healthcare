5e0842970616f68cdb6c3b79b88258bf57116566
RKeertish <131195123+RKeertish@users.noreply.github.com>
2023-10-21 22:24:37 +0530

    The data visualization was a success. New interesting plots and diagrams where formed after the code was working properly. xticks command was used to make the plots more readable with better labelling.

--- Datavis.py
+++ Datavis.py
@@ -2,49 +2,71 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load the preprocessed dataset into 'df' (make sure to replace 'preprocessed_stress_dataset.xlsx' with your file path)
+# Load the preprocessed dataset into 'df' (replace with your file path)
 df = pd.read_excel('preprocessed_stress_dataset.xlsx')
+plt.ioff()
 
-# Histogram for 'age'
-plt.figure(figsize=(8, 6))
+# Histograms for Numerical Variables
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 3, 1)
 sns.histplot(df['age'], kde=True)
 plt.title('Age Distribution')
 plt.xlabel('Age')
-plt.ylabel('Frequency')
+
+plt.subplot(1, 3, 2)
+sns.histplot(df['stress_at_home'], kde=True)
+plt.title('Stress at Home Distribution')
+plt.xlabel('Stress at Home')
+
+plt.subplot(1, 3, 3)
+sns.histplot(df['stress_at_work'], kde=True)
+plt.title('Stress at Work Distribution')
+plt.xlabel('Stress at Work')
+plt.tight_layout()
 plt.show()
 
-# Bar plot for 'Continent'
-plt.figure(figsize=(8, 6))
-sns.countplot(data=df, x='Continent')
-plt.title('Continent Distribution')
-plt.xlabel('Continent')
-plt.ylabel('Count')
-plt.xticks(rotation=45)
-plt.show()
-
-# Calculate the correlation matrix
-correlation_matrix = df.corr()
-
-# Create a heatmap
-plt.figure(figsize=(10, 8))
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=0.5)
-plt.title('Correlation Heatmap')
-plt.show()
-
-# Box plot for 'age'
-plt.figure(figsize=(8, 6))
+# Box Plots for Numerical Variables
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 3, 1)
 sns.boxplot(data=df, y='age')
 plt.title('Age Distribution (Box Plot)')
 plt.ylabel('Age')
+
+plt.subplot(1, 3, 2)
+sns.boxplot(data=df, y='stress_at_home')
+plt.title('Stress at Home (Box Plot)')
+plt.ylabel('Stress at Home')
+
+plt.subplot(1, 3, 3)
+sns.boxplot(data=df, y='stress_at_work')
+plt.title('Stress at Work (Box Plot)')
+plt.ylabel('Stress at Work')
+plt.tight_layout()
 plt.show()
 
-# Pair plot for numerical features
-sns.pairplot(df[['age', 'stress_at_home', 'stress_at_work']])
+# Count Plots for Categorical Variables
+plt.figure(figsize=(14, 4))
+plt.subplot(1, 3, 1)
+sns.countplot(data=df, x='Continent_Europe')
+plt.title('Continent Distribution')
+plt.xlabel('Continent')
+
+plt.subplot(1, 3, 2)
+sns.countplot(data=df, x='sex_nom_Female')
+plt.title('Gender Distribution')
+plt.xlabel('Gender')
+plt.xticks([0, 1], ['Female', 'Male'])
+
+plt.subplot(1, 3, 3)
+sns.countplot(data=df, x='work_normal_asusual_yn_usual')
+plt.title('Work Normality Distribution')
+plt.xlabel('Work Normality')
+plt.tight_layout()
 plt.show()
 
-# Count plot for 'sex_nom'
-sns.countplot(data=df, x='sex_nom')
-plt.title('Sex Distribution')
-plt.xlabel('Sex')
-plt.ylabel('Count')
+# Correlation Heatmap for Numerical Variables
+correlation_matrix = df[['age', 'stress_at_home', 'stress_at_work']].corr()
+plt.figure(figsize=(8, 6))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=0.5)
+plt.title('Correlation Heatmap')
 plt.show()
